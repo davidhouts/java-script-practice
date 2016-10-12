d0cfb945c0f13e5393d3a36617ec26aeaff14c67
davidhouts <dhouts@bluemodus.com>
2016-10-12 15:52:19 -0600

    Add files via upload

---
+++ java-script-practice/age-calculator.js
@@ -0,0 +1,5 @@
+var age = prompt ("How old are you?");
+alert ("You are roughly " + (age * 365) + " days old.");
+var years = prompt("What is your age?");
+var days = years * 365.25;
+alert (years + " years is roughly " + days + " days old counting leap years.");
