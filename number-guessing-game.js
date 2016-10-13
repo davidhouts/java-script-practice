7e1d7f6bd61f81c84c13179e316f200c7f273ef0
davidhouts <dhouts@bluemodus.com>
2016-10-13 12:10:17 -0600

    makes code more explicit

--- number-guessing-game.js
+++ number-guessing-game.js
@@ -2,15 +2,16 @@
 var num = 7;
 
 // Ask user for a number
-var number = prompt ("Enter a number to see if you win.")
+var stringNumber = prompt ("Enter a number to see if you win.");
+var number = Number(stringNumber);
 
 // Alert user that number has been guessed correctly
-if (Number(number) === num) {
+if (number === num) {
 	alert ("Great guess!  You win 50 Golden Tickets!");
 }
 
 // Alert user that number is too high
-else if (Number(number) > num) {
+else if (number > num) {
 	alert ("Too high. Guess again.");
 }
 
